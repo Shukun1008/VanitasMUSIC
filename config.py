9e371495193c7c108d6d8dbcb3ca8a0e1fd3d93b
Shukun1008 <shukun619@gmail.com>
2024-11-14 13:31:59 +0530

    Update1 config.py

--- config.py
+++ config.py
@@ -5,6 +5,48 @@ from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
+from telegram.ext import Updater, MessageHandler, Filters
+from google.cloud import vision
+import imagehash
+from PIL import Image
+
+def detect_copyright(file_path):
+    # Using Google Vision API
+    client = vision.ImageAnnotatorClient()
+    with open(file_path, "rb") as image_file:
+        content = image_file.read()
+    image = vision.Image(content=content)
+    response = client.web_detection(image=image)
+    # Custom logic to identify if content is copyrighted
+    if response.web_detection.full_matching_images:
+        return True  # Flag as copyrighted
+
+    # Perceptual Hashing for additional checks
+    known_hashes = {"known_hash_1", "known_hash_2"}  # Example set of known copyrighted hashes
+    image = Image.open(file_path)
+    img_hash = imagehash.phash(image)
+    if str(img_hash) in known_hashes:
+        return True  # Flag as copyrighted
+    return False
+
+def handle_media(update, context):
+    if update.message.photo or update.message.video or update.message.sticker:
+        file = update.message.photo[-1].get_file()  # Adjust for videos, stickers
+        file_path = file.download()
+        if detect_copyright(file_path):
+            update.message.delete()
+
+def main():
+    updater = Updater("YOUR_BOT_TOKEN", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.photo | Filters.video | Filters.sticker, handle_media))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+
+
 
 # Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID"))
@@ -24,49 +66,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6885582933))
 
-## Fill these variables if you're deploying on heroku.
-# Your heroku app name
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-# Get it from http://dashboard.heroku.com/account
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/arindam69x/New-music-bot69",
-)
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # Fill this variable if your upstream repository is private
-
-SUPPORT_CHANNEL = getenv("https://t.me/Anime_by_Eternals")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/beerussupportchat")
-
-# Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
-
-
-# Get this credentials from https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
-
-
-# Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
-
-
-# Telegram audio and video file size limit (in bytes)
-TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
-# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
-
-
-# Get your pyrogram v2 session from @StringFatherBot on Telegram
-STRING1 = getenv("STRING_SESSION", None)
-STRING2 = getenv("STRING_SESSION2", None)
-STRING3 = getenv("STRING_SESSION3", None)
-STRING4 = getenv("STRING_SESSION4", None)
-STRING5 = getenv("STRING_SESSION5", None)
+ 
 
 
 BANNED_USERS = filters.user()
@@ -77,37 +77,9 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/184a4ad6c5fc406ae2667.jpg"
-)
-PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/59d4eb516235811aee3d6.jpg"
-)
-PLAYLIST_IMG_URL = "https://telegra.ph/file/fe2d2876f1d206297025e.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/60f77d93e2d698a8508fb.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/941d949988c28982c15f1.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/07d8fd086c6bbdf1d9338.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/07d8fd086c6bbdf1d9338.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/07d8fd086c6bbdf1d9338.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/6d03c4e1793efac2bb48e.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/6d03c4e1793efac2bb48e.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/9d8acf1582a27adc9982b.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/9d8acf1582a27adc9982b.jpg"
 
 
-def time_to_seconds(time):
-    stringt = str(time)
-    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
 
-
-DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
-
-
-if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
-        raise SystemExit(
-            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
-        )
 
 if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
